11955e7a8a48544299fa086d5489d846996b9ace
Patrick <patryk@mercucio.pl>
2018-11-02 08:40:20 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // vue.config.js
 module.exports = {
-    // baseUrl: './', //for it's own hosting
-    baseUrl: 'http://www.osmkolo.pl/lomkolo.pl/', //for presentation
+    baseUrl: './',
+    /
 }
